edc64269b50fad7c87306ba3e4b195e7092e7e35
Illia Vernygora <illiavernigora@gmail.com>
2020-05-09 13:01:44 +0300

    clean the garbage in main.js file

--- Vue.js/find-the-pair/app/front/src/main.js
+++ Vue.js/find-the-pair/app/front/src/main.js
@@ -30,9 +30,6 @@ axios.interceptors.response.use(
   function (response) {
     return response;
   },
-  // error => (error.response.status === 401) ? router.push({
-  //   path: "/login"
-  // }) : Promise.reject(error)
   function(error) {
     if (error.response.status === 401) {      // Якщо сервер відповів «401»
       router.push({ path: "/login" });
